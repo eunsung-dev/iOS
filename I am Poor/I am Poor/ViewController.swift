651016a479647d0b2c5bb174577b35652f19ae3f
Eunsung_Choi <ens454@naver.com>
2021-08-14 01:47:56 +0900

    I am Poor

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by 최은성 on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
